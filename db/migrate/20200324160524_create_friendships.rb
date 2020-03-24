cc48794f42aeb6f1d3314023f09cc95db18cca16
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-03-24 11:45:12 -0500

    Fixed Foreign Key ERROR
    rqstuser foreign key required:true was causing a satabse error since it is referencing a key that is not instanciated in any table, fixed it by removing the foreign_key:true condition

--- db/migrate/20200324160524_create_friendships.rb
+++ db/migrate/20200324160524_create_friendships.rb
@@ -2,7 +2,7 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, foreign_key: true, index: true
-      t.references :rqstuser, foreign_key: true, index: true
+      t.references :rqstuser, index: true
       t.boolean :status
 
       t.timestamps null: false
