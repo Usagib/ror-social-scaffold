aca0fdbe99dec6048248f88b17a5963fd0b0616d
conejitomaya <eduardobonillac@gmail.com>
2020-03-26 10:21:31 -0600

    Friendship model testing methods could not be access

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
   before(:each) do
-    @friend1 = User.create(name: 'Friend1', email: 'sender@user.com', password: '123456')
-    @friend2 = User.create(name: 'Friend2', email: 'receiver@user.com', password: '123456')
+    @friend1 = User.create(name: 'Friend1', email: 'test1@friend.com', password: '123456')
+    @friend2 = User.create(name: 'Friend2', email: 'test2@friend.com', password: '123456')
   end
 
   context 'ActiveRecord Associations' do
@@ -12,6 +12,11 @@ RSpec.describe Friendship, type: :model do
   end
 
   context 'ApplicationRecord Test' do
-
+    it 'should confirm friendship' do
+      f = @friend1.friendships.build(rqstuser: @friend2, status: false)
+      f.save
+      confirm_friend(f.id)
+      expect(f.status).to be(true)
+    end
   end
 end
